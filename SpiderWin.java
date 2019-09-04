0db98901fb69b74e7a997d9438050f936b05d8b0
Mikayla Pasqualone <mpasqualone11@gmail.com>
2019-09-04 14:16:54 -0700

    Spider Win Screen Made :100:

---
+++ SpiderWin.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SpiderWin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SpiderWin extends World
+{
+
+    /**
+     * Constructor for objects of class SpiderWin.
+     * 
+     */
+    public SpiderWin()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
